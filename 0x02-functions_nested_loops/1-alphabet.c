f0a43774d7486a25d63162de3e37d8882f7e49d3
Tefe Mebuiefene <codestudioacademy@gmail.com>
2023-03-22 14:41:20 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,18 @@
 
 /**
  * print_alphabet - prints alphabet
- *
  * Description: The program prints the alphabet in lowercase.
  *followed by a new line.
- *
  * Return: Return O if there is no error in main program
  */
+
 void print_alphabet(void)
 {
-int n;
-for (n = 'a'; n <= 'z'; n++)
-{
- _putchar(n);
-}
- _putchar('\n');
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
 }
