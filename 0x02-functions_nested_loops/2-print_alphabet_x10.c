9dbdb75d2e9d20670ea98f89a86b29f8787a8de4
Tefe Mebuiefene <codestudioacademy@gmail.com>
2023-03-22 14:56:17 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Description: The program prints alphabets 10 times.
+ * followed by a new line
+ * Return: 0 if function  in main executes correctly
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (n = 'a'; n < 'z'; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
