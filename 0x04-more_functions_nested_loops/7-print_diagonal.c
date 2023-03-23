0e0276e1eb70157bd42de07b0116c4bf27112df5
Tefe Mebuiefene <codestudioacademy@gmail.com>
2023-03-23 09:05:20 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,16 @@ void print_diagonal(int n)
 
 	int j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		for (j = 0; j < i; j++)
-			_putchar(' ');
-		_putchar('\\');
-	}
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+		}
 	_putchar('\n');
+	}
+	else
+		_putchar('\n');
 }
