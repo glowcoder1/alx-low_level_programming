a0d98e8218b9e5e1303bf412aca1eae7f43a2cf1
Tefe Mebuiefene <codestudioacademy@gmail.com>
2023-03-23 08:07:01 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,19 +2,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number 0 - 9
- * Description: prints number 0123456789 followed by a new line
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
  * Return: void
  */
 
 void print_numbers(void)
 {
-	int n = 48;
+	int i;
 
-	while (n <= 57)
+	for (i = 48; i <= 57; i++)
 	{
-		_putchar(n);
-		n++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
