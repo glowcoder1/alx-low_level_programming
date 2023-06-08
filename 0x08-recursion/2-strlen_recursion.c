4051978985890ce263b69ebbb989f674c6fa2e2e
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-08 08:26:23 -0700

    lenght of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,6 +4,7 @@
  * _strlen_recursion - returns the length of a string
  * @s: string to determine its length
  *
+ * Return: int
  */
 
 int _strlen_recursion(char *s)
@@ -11,5 +12,9 @@ int _strlen_recursion(char *s)
 	if (*s)
 	{
 		return 1 + _strlen_recursion(s + 1);
+	}
+	else
+	{
+		return 0;
 	}
 }
