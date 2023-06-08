7b63aac57b438fd132fac8750204a8a6669d9bf5
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-08 07:27:00 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _print_rev_recursion - prints a string in reverse_
+ *@s: string to print
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+  if (*s)
+    {
+      _print_rev_recursion(s + 1);
+      _putchar(*s);
+    }
+  else
+    {
+      _putchar('\n');
+    }
+}
+
