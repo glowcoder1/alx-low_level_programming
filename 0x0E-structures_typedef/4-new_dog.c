621445ee0b9b56fcde71e9a70cebe2c200f2cb74
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-20 19:26:41 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,14 +7,22 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ * Return: a new dog if successful or NULL
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
+	char __attribute__((unused)) *name_copy;
+	char __attribute__((unused)) *owner_copy;
 
 	if (name == NULL || owner == NULL || my_dog == NULL)
+	{
+		free(my_dog);
 		return (NULL);
+	}
+	name_copy = name;
+	owner_copy = owner;
 	my_dog->name = name;
 	my_dog->age = age;
 	my_dog->owner = owner;
