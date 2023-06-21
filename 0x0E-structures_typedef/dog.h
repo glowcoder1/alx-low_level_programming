dd66a643921015ae83246163c58e4f7788abf0db
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-20 17:07:31 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog -data type for  dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+
+#endif
