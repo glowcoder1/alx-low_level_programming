406f010af94bba0d3fd3f9dfcc973bf92f1e09b0
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-20 19:00:10 -0700

    task 2 fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		return;
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	if (d->age >= 0.0 && d->age <= 150.0)
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 	else
 		printf("Age: %s\n", "(nill)");
 	printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
