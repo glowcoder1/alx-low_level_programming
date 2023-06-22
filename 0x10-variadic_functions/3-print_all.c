b31f78baff2c28d664317df6b21d746625417d19
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-22 16:52:53 -0700

    file structure for task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function
+ * @...: values to print
+ */
+
+void print_all(const char * const format, ...)
+{
+}
