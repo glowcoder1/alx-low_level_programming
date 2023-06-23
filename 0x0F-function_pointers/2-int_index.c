e9f557b0f43694c3f45adf7db7f77ef5b46f7235
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-23 16:27:50 -0700

    find index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: array to search
+ * @size: size of array
+ * @cmp: function to compare value
+ * Return: index of value if found else -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		int curr = array[i];
+
+		if (cmp(curr))
+			return (i);
+	}
+
+	return (-1);
+}
