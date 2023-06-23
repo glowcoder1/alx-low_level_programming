e0cbcbee3e2377ea94afe6fd2d06b6576fdbe361
Tefe Mebuiefene <glorytefe@gmail.com>
2023-06-23 16:15:46 -0700

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,21 +4,19 @@
  * array_iterator - executes a function given as a parameter on each element of
  * an array
  * @array: array conatining int
- * @size: size of array in bytes
+ * @size: size of array
  * @action: action to perform
  * Return: void
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int len;
 	unsigned int i;
 
 	if (action == NULL || size == 0 || array == NULL)
 		return;
-	len = size / 4;
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
